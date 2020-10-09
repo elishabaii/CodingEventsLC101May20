43a9db6d998170c4b57da3a61fc04a5505866bc2
elishabaii <eliernst@sbcglobal.net>
2020-10-08 20:03:50 -0500

    validation annotations video 3.1

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,18 +1,27 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
+    @NotBlank (message = "Please enter a name between 2 and 50 characters.")
+    @Size (min = 2, max = 50)
     private String name;
+    @Size(max = 500, message = "Description too long, please enter less then 500 characters.")
     private String description;
+    @Email(message = "Please enter a valid email address.")
+    private String contactEmail;
 
 
-    public Event(String name ,String description) {
+    public Event(String name ,String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
 
@@ -32,6 +41,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
